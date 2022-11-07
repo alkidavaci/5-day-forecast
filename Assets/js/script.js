4a33adb6594e94b1dd8af0eafd616ad087499faf
Alkida Vaci <alkida.vaci@gmail.com>
2022-11-07 01:45:45 -0500

    City added to the search history

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,5 @@
 // Declare an array of objects from local storage 
-var newCities = JSON.parse(localStorage.getItem("newCity"))||[];
+var newCities = JSON.parse(localStorage.getItem("newCity")) || [];
 
 // Define variable
 var cityFormEl = document.querySelector('#city-form');
@@ -26,10 +26,10 @@ function mpsToMph(mps) {
 function formSubmitCity(e) {
     e.preventDefault();
     var cityName = cityInputEl.value.trim();
-    
+
     // User type a city name
     if (cityName) {
-        getUsersLonLat(cityName);         
+        getUsersLonLat(cityName);
     } else {
         alert('Please enter a city name');
     }
@@ -37,7 +37,10 @@ function formSubmitCity(e) {
 
 //Display current day data
 function displayTodayData(data) {
-     storeCities(data); 
+
+    // Call function to display data from local storage
+    storeCities(data);
+
 
     // Set attribute for div id=today-weather
     todayContainerEl.setAttribute("class", "today-weather")
@@ -46,7 +49,7 @@ function displayTodayData(data) {
     var cityDisplayEl = document.createElement('h2');
     cityDisplayEl.textContent = data.city.name;
     todayContainerEl.appendChild(cityDisplayEl);
-     
+
 
     // Create span tag, place text city name, append tag in the h2
     var dateDisplayEl = document.createElement('span');
@@ -54,14 +57,14 @@ function displayTodayData(data) {
     cityDisplayEl.appendChild(dateDisplayEl);
 
     // Create img tag inside a span tag, set attribute src the link for icon, append span tag in the h2
-    var iconSpan =  document.createElement('span');
+    var iconSpan = document.createElement('span');
     var iconDisplayEl = document.createElement('img');
     var iconCode = data.list[0].weather[0].icon;
     var iconUrl = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
     iconDisplayEl.setAttribute('src', iconUrl);
     iconSpan.appendChild(iconDisplayEl);
     cityDisplayEl.appendChild(iconSpan);
-    
+
     // Create h3 tag, place text temperature in Fahrenheit, append tag in the h3            
     var tempDisplayEl = document.createElement('h4');
     tempDisplayEl.textContent = 'Temp: ' + kelvinToFahrenheit(data.list[0].main.temp) + ' \u{2109}';
@@ -80,44 +83,44 @@ function displayTodayData(data) {
 
 // Display five day data
 function displayFiveData(data) {
-     // Set attribute for div id=today-weather
-     fiveDayContainerEl.setAttribute("class", "five-day-weather")
-     fiveDayH2.classList.remove('hidden');
-        
-      
-    for(let i = 1; i < 6; i++){
-    var fiveDayEl = document.createElement('div');
-    
+    // Set attribute for div id=today-weather
+    fiveDayContainerEl.setAttribute("class", "five-day-weather")
+    fiveDayH2.classList.remove('hidden');
 
-    // Create span tag, place text city name, append tag in the h2
-    var dateDisplayEl = document.createElement('h3');
-    dateDisplayEl.textContent = moment(data.list[i].dt_txt).format('L');
-    fiveDayEl.appendChild(dateDisplayEl);
 
-    // Create img tag inside a span tag, set attribute src the link for icon, append span tag in the h2
-    var iconDisplayEl = document.createElement('img');
-    var iconCode = data.list[i].weather[0].icon;
-    var iconUrl = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
-    iconDisplayEl.setAttribute('src', iconUrl);
-    fiveDayEl.appendChild(iconDisplayEl);
-    
-    // Create h3 tag, place text temperature in Fahrenheit, append tag in the h3            
-    var tempDisplayEl = document.createElement('h4');
-    tempDisplayEl.textContent = 'Temp: ' + kelvinToFahrenheit(data.list[i].main.temp) + ' \u{2109}';
-    fiveDayEl.appendChild(tempDisplayEl);
+    for (let i = 1; i < 6; i++) {
+        var fiveDayEl = document.createElement('div');
 
-    // Create h3 tag, place text wind in MPH, append tag in the h3 
-    var windDisplayEl = document.createElement('h4');
-    windDisplayEl.textContent = 'Wind: ' + mpsToMph(data.list[i].wind.speed) + ' MPH';
-    fiveDayEl.appendChild(windDisplayEl);
 
-    // Create h3 tag, place text humidity percentage, append tag in the h3 
-    var humidityDisplayEl = document.createElement('h4');
-    humidityDisplayEl.textContent = 'Humidity: ' + data.list[i].main.humidity + ' \u{0025}';
-    fiveDayEl.appendChild(humidityDisplayEl);
+        // Create span tag, place text city name, append tag in the h2
+        var dateDisplayEl = document.createElement('h3');
+        dateDisplayEl.textContent = moment(data.list[i].dt_txt).format('L');
+        fiveDayEl.appendChild(dateDisplayEl);
 
-    fiveDayContainerEl.appendChild(fiveDayEl);
-}
+        // Create img tag inside a span tag, set attribute src the link for icon, append span tag in the h2
+        var iconDisplayEl = document.createElement('img');
+        var iconCode = data.list[i].weather[0].icon;
+        var iconUrl = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+        iconDisplayEl.setAttribute('src', iconUrl);
+        fiveDayEl.appendChild(iconDisplayEl);
+
+        // Create h3 tag, place text temperature in Fahrenheit, append tag in the h3            
+        var tempDisplayEl = document.createElement('h4');
+        tempDisplayEl.textContent = 'Temp: ' + kelvinToFahrenheit(data.list[i].main.temp) + ' \u{2109}';
+        fiveDayEl.appendChild(tempDisplayEl);
+
+        // Create h3 tag, place text wind in MPH, append tag in the h3 
+        var windDisplayEl = document.createElement('h4');
+        windDisplayEl.textContent = 'Wind: ' + mpsToMph(data.list[i].wind.speed) + ' MPH';
+        fiveDayEl.appendChild(windDisplayEl);
+
+        // Create h3 tag, place text humidity percentage, append tag in the h3 
+        var humidityDisplayEl = document.createElement('h4');
+        humidityDisplayEl.textContent = 'Humidity: ' + data.list[i].main.humidity + ' \u{0025}';
+        fiveDayEl.appendChild(humidityDisplayEl);
+
+        fiveDayContainerEl.appendChild(fiveDayEl);
+    }
 }
 
 //  Functionality to get users weather data from the input city name
@@ -166,8 +169,8 @@ var getUsersCity = function (userCityLat, userCityLon) {
                         displayTodayData(data);
                     }
 
-                       // If div id=five-day-weather is empty, display five data
-                       if (fiveDayContainerEl === "") {
+                    // If div id=five-day-weather is empty, display five data
+                    if (fiveDayContainerEl === "") {
                         displayFiveData(data);
                     }
                     else {
@@ -186,15 +189,37 @@ var getUsersCity = function (userCityLat, userCityLon) {
         });
 };
 
-function storeCities(data){
+// Set in local storage
+function storeCities(data) {
 
-    var newCityLower = data.city.name   
-    // Push in array new city
-   newCities.push(newCityLower)
-     // Set city name in local storage
-     localStorage.setItem("newCity", newCities)
+    var newCityName = data.city.name;
+
+    if (!newCities.includes(newCityName)) {
+
+        // Push in array new city
+        newCities.unshift(newCityName);
+        // Set city name in local storage
+        localStorage.setItem("newCity", JSON.stringify(newCities));
+        console.log(newCities)
+
     }
+    // Empty div that contains button of the cities
+    citiesContainerEl.innerHTML = ""
+    // Call function to display button of the cities 
+    getInputCities(newCities)
 
+}
+
+// Display from local storage
+function getInputCities(array) {
+
+    for (let i = 0; i < array.length; i++) {
+        var newButton = document.createElement('button');
+        newButton.setAttribute('class', 'btn btn-secondary col-12')
+        newButton.textContent = array[i];
+        citiesContainerEl.appendChild(newButton);
+    }
+}
 
 // Event listener to submit the city
 cityFormEl.addEventListener('submit', formSubmitCity);
